982a468db31f59633e8c7f51d759fc4f139cbdba
angelee otic <otic.angeleemae@gmail.com>
2023-01-12 23:42:33 +0000

    added seeds, show index in controller, views for it too

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "doctors", to: "doctors#index"
+  get 'doctors', to: 'doctors#index'
+
+  get 'doctors/:id', to: 'doctors#show', as: :doctor
 end
